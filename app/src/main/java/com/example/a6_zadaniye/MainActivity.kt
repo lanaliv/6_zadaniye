5b2d5003e777830f0f8170ad081e93a6b7ae5f4e
lanaliv <livadnaia@gmail.com>
2022-10-11 17:12:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a6_zadaniye/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a6_zadaniye
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
